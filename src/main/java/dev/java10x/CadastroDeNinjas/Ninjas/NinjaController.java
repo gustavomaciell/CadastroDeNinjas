37e4b7b553ec8bf56572a343368183e5a11567e3
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-07-17 16:34:20 -0300

    feat: Cria as interfaces NinjaRepository e MissõesRepository e as classes NinjaService e MissoesController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NinjaController {
 
     @GetMapping("/boasvindas")
-    public String boasVindas(){
+    public String boasVindas() {
         return "Esta é minha primeira mensagem nessa rota.";
     }
 }

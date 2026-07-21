3cd5f7453b0174c683ccf3206e22bf5662458113
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-07-20 22:12:56 -0300

    feat: Altera a rota do Controller para listar as missoes e injeta a dependencia do SERVICE

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,14 +2,26 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
 
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     //GET -> Mandar uma requisicao para listar as missoes
     @GetMapping("/listar")
-    public String listarMissoes() {
-        return "Missões listadas com sucesso";
+    public List<MissoesModel> listarMissoes() {
+        return missoesService.listarMissoes();
+    }
+    @GetMapping("/listar/{id}")
+    public MissoesModel missoesModel(@PathVariable Long id){
+        return missoesService.listarMissoesPorId(id);
     }
 
     //POST -> Mandar uma requisiçao para criar as missoes
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -2,8 +2,10 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MissoesService {
@@ -16,5 +18,10 @@ public class MissoesService {
 
     public List<MissoesModel> listarMissoes() {
         return missoesRepository.findAll();
+    }
+
+    public MissoesModel listarMissoesPorId(Long id) {
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
     }
 }

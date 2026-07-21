65873b32da5631d64a64358cc0c428455abb3962
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-07-20 21:54:05 -0300

    feat: Cria a classe MissoesService e injeta a dependencia do REPOSITORY

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -0,0 +1,20 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+}
